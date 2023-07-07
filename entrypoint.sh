ff9c112b1fb701b0ba391964cbe47ba34cb4f423
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2023-07-07 11:55:55 +0200

    Modify bash script to support prefix input

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ EXPORT_TO_CI_ENV=$3
 EXPORT_TO_CI_OUTPUTS=$4
 EXPORT_TO_ENV_FILE=$5
 ENV_FILE=$6
+PREFIX=$7
 
 echo "SOURCE_FILE: $SOURCE_FILE"
 echo "YAML_PROPERTIES: $YAML_PROPERTIES"
@@ -13,6 +14,8 @@ echo "EXPORT_TO_CI_ENV: $EXPORT_TO_CI_ENV"
 echo "EXPORT_TO_CI_OUTPUTS: $EXPORT_TO_CI_OUTPUTS"
 echo "EXPORT_TO_ENV_FILE: $EXPORT_TO_ENV_FILE"
 echo "ENV_FILE: $ENV_FILE"
+echo "PREFIX: $PREFIX"
+
 
 #YQ_PATH=$1 /usr/bin/yq 'eval(strenv(YQ_PATH))' .cicd/metadata.yaml
 
@@ -35,7 +38,7 @@ do
 
     if [ "$EXPORT_TO_CI_ENV" = "true" ]; then
         echo "Exporting to CI env"
-        echo "$key=$YQ_RESULT" >> $GITHUB_ENV
+        echo "${PREFIX}${key}=$YQ_RESULT" >> $GITHUB_ENV
     fi
 
     if [ "$EXPORT_TO_CI_OUTPUTS" = "true" ]; then
@@ -45,7 +48,7 @@ do
 
     if [ "$EXPORT_TO_ENV_FILE" = "true" ]; then
         echo "Exporting to env file"
-        echo "$key=$YQ_RESULT" >> $ENV_FILE
+        echo "${PREFIX}${key}=$YQ_RESULT" >> $ENV_FILE
     fi
 
 done
